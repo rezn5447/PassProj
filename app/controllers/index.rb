afb58fe02be463c6e9e45bf130c702ac66d3bf01
Resdan Logan <email": null>
2016-10-11 18:59:43 -0700

    login logic

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,13 +3,48 @@ get '/' do
   erb :index
 end
 
+get '/session-viewer' do
+  session.inspect
+end
 
 get '/login' do
   erb :login
 end
 
-get '/signup' do
-  erb :signup
+# post '/login' do
+# erb :"commanders/login"
+#  @commander = Commander.find(params[:id])
+#  erb :"commanders/show"
+# end
+# get '/signup' do
+#   erb :signup
+# end
+
+post '/login' do
+  @commander = Commander.find_by_email(params[:email])
+  if @commander.password == params[:password]
+       session[:id] = @commander.id
+      redirect '/logged_in'
+  else
+    erb :"login"
+  end
+end
+
+get '/logged_in' do
+  erb :"logged_in"
+end
+
+get '/logged_out' do
+  session.delete(:id)
+  redirect '/'
+end
+
+get '/secret' do
+  if  session[:id]
+   erb :"secret"
+ else
+  redirect '/'
+ end
 end
 
 # CONTROLLERS I NEED TO ADD
